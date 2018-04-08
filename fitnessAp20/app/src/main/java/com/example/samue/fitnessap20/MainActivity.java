94775f47277a2295c859f26b7dbf5890ac1b461b
Sam Milward <samalexmilward@gmail.com>
2018-04-08 14:24:42 +0100

    Modified MainActivity to display current user, and give options to continue with this user, or to wipe database and create another user

--- fitnessAp20/app/src/main/java/com/example/samue/fitnessap20/MainActivity.java
+++ fitnessAp20/app/src/main/java/com/example/samue/fitnessap20/MainActivity.java
@@ -4,19 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseManager dm = new DatabaseManager(this);
+    TextView lblCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lblCurrentUser = (TextView)findViewById(R.id.lblCurrentUser);
+        loadData();
+    }
+
+    protected void loadData()
+    {
+        lblCurrentUser.setText(dm.GetUsersDetails().Name);
     }
 
 
     protected void butPressed(View view){
 
-            if(view == findViewById(R.id.button)){
+            if(view == findViewById(R.id.cmdContiinue)){
                 Intent activity = new Intent(getApplication(), HomeActivity.class);
 
                 startActivity(activity);
