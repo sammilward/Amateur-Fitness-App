0e9b8235caa2a18bd4418ac8dfd56c08fe7200ad
Sam Milward <samalexmilward@gmail.com>
2018-04-13 12:36:21 +0100

    Added all exercises to textfile to be loaded to database, named all the images

--- fitnessAp20/app/src/main/java/com/example/samue/fitnessap20/ExerciseActivity.java
+++ fitnessAp20/app/src/main/java/com/example/samue/fitnessap20/ExerciseActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class ExerciseActivity extends AppCompatActivity {
 
+    DatabaseManager dm = new DatabaseManager(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+        dm.loadExercises(this);
     }
 }
